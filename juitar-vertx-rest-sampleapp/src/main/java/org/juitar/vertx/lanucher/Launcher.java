b702e6163aae568cdbd14de8c88524d5728cb778
sha1n <shai.nagar@gmail.com>
2013-01-31 23:41:08 +0200

    Added JDBC worker use-case using the jdbc-worker module from "juitar-playground"

--- juitar-vertx-rest-sampleapp/src/main/java/org/juitar/vertx/lanucher/Launcher.java
+++ juitar-vertx-rest-sampleapp/src/main/java/org/juitar/vertx/lanucher/Launcher.java
@@ -1,10 +1,13 @@
 package org.juitar.vertx.lanucher;
 
+import junitar.server.netty.SpringContextLoader;
 import org.glassfish.jersey.server.ApplicationHandler;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.internal.scanning.PackageNamesScanner;
 import org.juitar.server.http.vertx.VertxHttpServer;
 import org.juitar.vertx.jersey.VertxContainerHandler;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,9 +17,13 @@ import java.net.URISyntaxException;
  * @author sha1n
  * Date: 1/29/13
  */
-public class Lanucher {
+public class Launcher {
+
+    public static ApplicationContext applicationContext;
 
     public static void main(String... args) throws IOException, URISyntaxException {
+
+        GenericApplicationContext genericApplicationContext = null;
 
         ResourceConfig resourceConfig = new ResourceConfig();
         resourceConfig.registerFinder(new PackageNamesScanner(
@@ -32,5 +39,9 @@ public class Lanucher {
                         new ApplicationHandler(resourceConfig)));
 
         httpServer.start();
+        genericApplicationContext = new SpringContextLoader().load();
+        genericApplicationContext.refresh();
+        genericApplicationContext.start();
+        applicationContext = genericApplicationContext;
     }
 }
