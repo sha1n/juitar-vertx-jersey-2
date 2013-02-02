66b6656b0cf8af9917af73121396231103ade98a
sha1n <shai.nagar@gmail.com>
2013-02-02 02:01:09 +0200

    Started playing with JMX..

--- juitar-vertx-rest-sampleapp/src/main/java/org/juitar/vertx/lanucher/Launcher.java
+++ juitar-vertx-rest-sampleapp/src/main/java/org/juitar/vertx/lanucher/Launcher.java
@@ -1,19 +1,12 @@
 package org.juitar.vertx.lanucher;
 
 import junitar.server.netty.SpringContextLoader;
-import org.glassfish.jersey.server.ApplicationHandler;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.internal.scanning.PackageNamesScanner;
-import org.juitar.server.http.vertx.VertxHttpServer;
-import org.juitar.vertx.jersey.VertxContainerHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
-import org.vertx.java.core.http.RouteMatcher;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.ExecutionException;
 
@@ -37,24 +30,25 @@ public class Launcher {
         genericApplicationContext.start();
         applicationContext = genericApplicationContext;
 
-        ResourceConfig resourceConfig = new ResourceConfig();
-        resourceConfig.registerFinder(new PackageNamesScanner(
-                new String[]{
-                        "org.juitar.web.rest.resources",
-                }, true));
-
-
-        RouteMatcher routeMatcher = new RouteMatcher();
-        routeMatcher.all(
-                "/api/.*",
-                new VertxContainerHandler(
-                        new URI("http://localhost:8080/api/"),
-                        new ApplicationHandler(resourceConfig)));
-
-
-        VertxHttpServer httpServer = new VertxHttpServer(8080, routeMatcher, new AppShutdownHook());
-        httpServer.start();
-
+//        ResourceConfig resourceConfig = new ResourceConfig();
+//        resourceConfig.registerFinder(new PackageNamesScanner(
+//                new String[]{
+//                        "org.juitar.web.rest.resources",
+//                }, true));
+//
+//
+//        RouteMatcher routeMatcher = new RouteMatcher();
+//        routeMatcher.all(
+//                "/api/.*",
+//                new VertxContainerHandler(
+//                        new URI("http://localhost:8080/api/"),
+//                        new ApplicationHandler(resourceConfig)));
+//
+//
+//        VertxHttpServer httpServer = new VertxHttpServer(8080, routeMatcher, new AppShutdownHook());
+//        httpServer.start();
+        HttpServerJmxWrapper httpServerJmxWrapper = applicationContext.getBean(HttpServerJmxWrapper.class);
+        httpServerJmxWrapper.start();
         LOGGER.info("Server is ready.");
     }
 }
