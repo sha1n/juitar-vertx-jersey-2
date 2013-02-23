eed7126a4cde24e1911feacee74fbdf24e00c0ec
sha1n <sha1n@users.noreply.github.com>
2013-02-23 22:08:46 +0200

    Minor: some adjustments for the benchmark.

--- sample-rest-app/src/main/java/org/juitar/web/rest/resources/SyncWorkerQueueResource.java
+++ sample-rest-app/src/main/java/org/juitar/web/rest/resources/SyncWorkerQueueResource.java
@@ -3,15 +3,15 @@ package org.juitar.web.rest.resources;
 import org.juitar.monitoring.api.MethodInvocationProbe;
 import org.juitar.spring.SpringContextLoader;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * @author sha1n
@@ -28,26 +28,33 @@ public class SyncWorkerQueueResource {
     public String submit(String sql) throws SQLException {
 
         if (PUT_PROBE.hit()) {
-            System.out.println("PUT TPS: " + PUT_PROBE.getLastInvocationCount());
+            System.out.println("SYNC PUT TPS: " + PUT_PROBE.getLastInvocationCount());
         }
 
         ApplicationContext applicationContext = SpringContextLoader.getContext();
+        JdbcTemplate jdbc = applicationContext.getBean(JdbcTemplate.class);
 
-        DataSource dataSource = applicationContext.getBean(DataSource.class);
-
-        try (
-                Connection connection = dataSource.getConnection();
-                Statement statement = connection.createStatement()) {
-
-            connection.setAutoCommit(false);
-            int count = statement.executeUpdate(sql);
-            connection.commit();
-            return String.format("Updated %d records", count);
+        try {
+            return String.format("Updated %d records", jdbc.update(sql));
         } catch (Exception e) {
             return "ERROR: " + e.getMessage() + " Statement: " + sql;
         }
     }
 
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/sql")
+    public String count() {
+
+        ApplicationContext applicationContext = SpringContextLoader.getContext();
+        JdbcTemplate jdbc = applicationContext.getBean(JdbcTemplate.class);
+
+        try {
+            return String.format("Count = %d", jdbc.queryForInt("SELECT COUNT(1) FROM TEST"));
+        } catch (DataAccessException e) {
+            return "ERROR: " + e.getMessage();
+        }
+    }
 
 }
 
